b034ac484b15789340534a2cff3167d8dcf02b8a
Atar Mor <atarm@moveohls.com>
2024-07-31 13:48:52 +0300

    feat: add generic tabs cmp

---
+++ src/components/generic-components/tabs/Tabs.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import GenericTabs from './Tabs'
+import { CardsIcon, ListIcon } from '../../../assets/svg/svg';
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<typeof GenericTabs> = {
+  component: GenericTabs,
+};
+
+export default meta;
+type Story = StoryObj<typeof GenericTabs>;
+
+export const FirstStory: Story = {
+  args: {
+    //👇 The args you need here will depend on your component
+    tabs: [
+      { label: 'List', content: 'List', icon: <ListIcon /> },
+      { label: 'Cards', content: 'Cards', icon: <CardsIcon /> },
+    ]
+  },
+};
---
+++ src/components/generic-components/tabs/Tabs.tsx
@@ -0,0 +1,93 @@
+import React, { ReactElement, ReactNode, SyntheticEvent, useState } from 'react';
+import { Tabs, Tab, Box } from '@mui/material';
+import { colors, primaryFont, textStyle } from '../../../assets/style/setup/constants';
+
+interface TabItem {
+    label: string
+    content: ReactNode
+    icon?: ReactNode // Optional icon for each tab
+}
+
+interface GenericTabsProps {
+    tabs: TabItem[]
+}
+
+export function GenericTabs({ tabs }: GenericTabsProps) {
+    const [value, setValue] = useState(0)
+
+    const handleChange = (event: SyntheticEvent, newValue: number) => {
+        setValue(newValue)
+    }
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                sx={{
+                    borderBottom: 0,
+                    '.MuiTab-root': { minHeight: 'auto' },
+                    '& .MuiTabs-indicator': {
+                        backgroundColor: colors.neutrals.black
+                    },
+                    '.MuiTabs-scroller': {
+                        height: 'fit-content'
+                    }
+                }}
+            >
+                {tabs.map((tab, index) => (
+                    <Tab
+                        key={index}
+                        label={tab.label}
+                        icon={tab.icon as ReactElement} // Render the SVG icon
+                        iconPosition="start" // Position the icon before the label
+                        style={{ minWidth: 'auto', height: '38px', padding: '4px 8px' }}
+                        sx={{
+                            gap: '8px',
+                            // paddingInline: '0',
+                            fontFamily: primaryFont.regular,
+                            ...textStyle.body,
+                            textTransform: 'none',
+                            color: colors.neutrals.black,
+                            '&.Mui-selected': {
+                                fontFamily: primaryFont.bold,
+                                color: colors.neutrals.black,
+                            },
+                            '&:hover': {
+                                backgroundColor: colors.primary[50],
+                            }
+                        }}
+                    />
+                ))}
+            </Tabs>
+            {tabs.map((tab, index) => (
+                <TabPanel key={index} value={value} index={index}>
+                    {tab.content}
+                </TabPanel>
+            ))}
+        </Box>
+    )
+}
+
+interface TabPanelProps {
+    children: React.ReactNode
+    index: number
+    value: number
+}
+
+const TabPanel = ({ children, value, index }: TabPanelProps) => {
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    )
+}
